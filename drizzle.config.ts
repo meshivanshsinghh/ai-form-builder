661003e9a109094f76a85eaf8bd8b2328cb6272f
Shivansh  Singh <shivanshsingh@Shivanshs-MacBook-Air.local>
2024-04-24 15:01:09 +0530

    successfully created drizzle orm schema and connected supabase postgress database

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "drizzle-kit";
+
+// needed for drizzle
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString:
+      process.env.DATABASE_URL ||
+      "postgres://postgres:postgres@localhost:5432/postgres",
+  },
+} satisfies Config;
